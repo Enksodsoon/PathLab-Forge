6f5beb27de776677d1dccbbf6bbd32ca7a3dee6f
Enksodsoon <enk44139@gmail.com>
2026-07-30 19:48:53 +0700

    test: make resource checks cross-platform

--- src/test/java/org/pathlab/forge/runtime/ChildProcessContainmentTest.java
+++ src/test/java/org/pathlab/forge/runtime/ChildProcessContainmentTest.java
@@ -8,12 +8,15 @@ import org.junit.jupiter.api.Test;
 final class ChildProcessContainmentTest {
     @Test
     void terminatesRegisteredChildTreeOnContainmentClose() throws Exception {
-        var process = new ProcessBuilder(
-                        "powershell.exe",
-                        "-NoProfile",
-                        "-Command",
-                        "Start-Sleep -Seconds 30")
-                .start();
+        var windows = System.getProperty("os.name", "").startsWith("Windows");
+        var process = windows
+                ? new ProcessBuilder(
+                                "powershell.exe",
+                                "-NoProfile",
+                                "-Command",
+                                "Start-Sleep -Seconds 30")
+                        .start()
+                : new ProcessBuilder("sh", "-c", "sleep 30").start();
         try (var containment = new ChildProcessContainment()) {
             containment.register(process);
         }
